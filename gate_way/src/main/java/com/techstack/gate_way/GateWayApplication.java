ed8f2892cdc01c52f4f36e1c18cf4f3c4e52763b
Minh Nguyen <nguyenlehienminh@gmail.com>
2021-08-10 13:52:52 +0700

    Dockerized services

--- gate_way/src/main/java/com/techstack/gate_way/GateWayApplication.java
+++ gate_way/src/main/java/com/techstack/gate_way/GateWayApplication.java
@@ -3,10 +3,8 @@ package com.techstack.gate_way;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableEurekaClient
 public class GateWayApplication {
     
--- user_service/src/main/java/com/techstack/user_service/UserServiceApplication.java
+++ user_service/src/main/java/com/techstack/user_service/UserServiceApplication.java
@@ -3,7 +3,6 @@ package com.techstack.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableEurekaClient
 @SpringBootApplication
